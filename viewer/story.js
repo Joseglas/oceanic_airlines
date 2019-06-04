655f353862dbdc8de15704b1a13912f8e7b0cf1f
Jose Glas <39164416+Joseglas@users.noreply.github.com>
2019-06-04 03:24:11 -0300

    primer
    1er commit

---
+++ viewer/story.js
@@ -0,0 +1,931 @@
+var story = {
+"docName": "ui-ife",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": true,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "home.png",
+"image2x": "home@2x.png",
+"width": 1024,
+"height": 768,
+"title": "Home",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1024,
+		"height": 68,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "home-0.png",
+		"image2x": "home-0@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 648,
+			"y": 400,
+			"width": 241,
+			"height": 134
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 186,
+			"y": 191,
+			"width": 140,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 1,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 348,
+			"y": 191,
+			"width": 140,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 1,
+"image": "home_ingles.png",
+"image2x": "home_ingles@2x.png",
+"width": 1024,
+"height": 768,
+"title": "Home ingles",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1024,
+		"height": 68,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "home_ingles-0.png",
+		"image2x": "home_ingles-0@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 648,
+			"y": 400,
+			"width": 241,
+			"height": 134
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 24,
+			"y": 191,
+			"width": 140,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 348,
+			"y": 191,
+			"width": 140,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 2,
+"image": "home_italiano.png",
+"image2x": "home_italiano@2x.png",
+"width": 1024,
+"height": 768,
+"title": "Home italiano",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1024,
+		"height": 68,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "home_italiano-0.png",
+		"image2x": "home_italiano-0@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 648,
+			"y": 400,
+			"width": 241,
+			"height": 134
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 24,
+			"y": 191,
+			"width": 140,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 186,
+			"y": 191,
+			"width": 140,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 1,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 3,
+"image": "guide.png",
+"image2x": "guide@2x.png",
+"width": 1024,
+"height": 768,
+"title": "Guide",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1024,
+		"height": 68,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [
+			{
+				"rect": {
+					"x": 0,
+					"y": 0,
+					"width": 1024,
+					"height": 68
+				},
+				"isParentFixed": true,
+				"page": 8,
+				"index": 0
+			}
+		],
+		"image": "guide-0.png",
+		"image2x": "guide-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 728,
+		"width": 1024,
+		"height": 40,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "guide-1.png",
+		"image2x": "guide-1@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 0,
+			"y": 68,
+			"width": 600,
+			"height": 660
+		},
+		"isParentFixed": false,
+		"page": 4,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 4,
+"image": "item_details_girls.png",
+"image2x": "item_details_girls@2x.png",
+"width": 1024,
+"height": 768,
+"title": "Item Details girls",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1024,
+		"height": 68,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [
+			{
+				"rect": {
+					"x": 0,
+					"y": 0,
+					"width": 1024,
+					"height": 68
+				},
+				"isParentFixed": true,
+				"page": 8,
+				"index": 0
+			}
+		],
+		"image": "item_details_girls-0.png",
+		"image2x": "item_details_girls-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 728,
+		"width": 1024,
+		"height": 40,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "item_details_girls-1.png",
+		"image2x": "item_details_girls-1@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 558,
+			"y": 668,
+			"width": 200,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"page": 7,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 770,
+			"y": 68,
+			"width": 250,
+			"height": 190
+		},
+		"isParentFixed": false,
+		"page": 5,
+		"index": 2
+	},
+	{
+		"rect": {
+			"x": 770,
+			"y": 262,
+			"width": 250,
+			"height": 190
+		},
+		"isParentFixed": false,
+		"page": 6,
+		"index": 3
+	},
+	{
+		"rect": {
+			"x": 18,
+			"y": 86,
+			"width": 52,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"action": "back",
+		"index": 4
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 5,
+"image": "item_details_map.png",
+"image2x": "item_details_map@2x.png",
+"width": 1024,
+"height": 768,
+"title": "Item Details map",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1024,
+		"height": 68,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [
+			{
+				"rect": {
+					"x": 0,
+					"y": 0,
+					"width": 1024,
+					"height": 68
+				},
+				"isParentFixed": true,
+				"page": 8,
+				"index": 0
+			}
+		],
+		"image": "item_details_map-0.png",
+		"image2x": "item_details_map-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 728,
+		"width": 1024,
+		"height": 40,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "item_details_map-1.png",
+		"image2x": "item_details_map-1@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 558,
+			"y": 668,
+			"width": 200,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"page": 7,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 770,
+			"y": 262,
+			"width": 250,
+			"height": 190
+		},
+		"isParentFixed": false,
+		"page": 6,
+		"index": 2
+	},
+	{
+		"rect": {
+			"x": 770,
+			"y": 456,
+			"width": 250,
+			"height": 190
+		},
+		"isParentFixed": false,
+		"page": 4,
+		"index": 3
+	},
+	{
+		"rect": {
+			"x": 18,
+			"y": 86,
+			"width": 52,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"action": "back",
+		"index": 4
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 6,
+"image": "item_details_kids.png",
+"image2x": "item_details_kids@2x.png",
+"width": 1024,
+"height": 768,
+"title": "Item Details kids",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1024,
+		"height": 68,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [
+			{
+				"rect": {
+					"x": 0,
+					"y": 0,
+					"width": 1024,
+					"height": 68
+				},
+				"isParentFixed": true,
+				"page": 8,
+				"index": 0
+			}
+		],
+		"image": "item_details_kids-0.png",
+		"image2x": "item_details_kids-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 728,
+		"width": 1024,
+		"height": 40,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "item_details_kids-1.png",
+		"image2x": "item_details_kids-1@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 558,
+			"y": 668,
+			"width": 200,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"page": 7,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 770,
+			"y": 68,
+			"width": 250,
+			"height": 190
+		},
+		"isParentFixed": false,
+		"page": 5,
+		"index": 2
+	},
+	{
+		"rect": {
+			"x": 18,
+			"y": 86,
+			"width": 52,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"action": "back",
+		"index": 3
+	},
+	{
+		"rect": {
+			"x": 770,
+			"y": 456,
+			"width": 250,
+			"height": 190
+		},
+		"isParentFixed": false,
+		"page": 4,
+		"index": 4
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 7,
+"image": "item_details_checkout.png",
+"image2x": "item_details_checkout@2x.png",
+"width": 1024,
+"height": 768,
+"title": "Item Details checkout",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1024,
+		"height": 68,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [
+			{
+				"rect": {
+					"x": 0,
+					"y": 0,
+					"width": 1024,
+					"height": 68
+				},
+				"isParentFixed": true,
+				"page": 8,
+				"index": 0
+			}
+		],
+		"image": "item_details_checkout-0.png",
+		"image2x": "item_details_checkout-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 728,
+		"width": 1024,
+		"height": 40,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "item_details_checkout-1.png",
+		"image2x": "item_details_checkout-1@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 18,
+			"y": 86,
+			"width": 52,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"action": "back",
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 624,
+			"y": 636,
+			"width": 368,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"page": 9,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 8,
+"image": "home_notificatin.png",
+"image2x": "home_notificatin@2x.png",
+"width": 1024,
+"height": 768,
+"title": "Home Notificatin",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'modal',
+'showShadow': 1,
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 0,
+			"y": 0,
+			"width": 1024,
+			"height": 768
+		},
+		"isParentFixed": false,
+		"action": "back",
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 9,
+"image": "succefull.png",
+"image2x": "succefull@2x.png",
+"width": 1024,
+"height": 768,
+"title": "succefull",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1024,
+		"height": 68,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [
+			{
+				"rect": {
+					"x": 0,
+					"y": 0,
+					"width": 1024,
+					"height": 68
+				},
+				"isParentFixed": true,
+				"page": 8,
+				"index": 0
+			}
+		],
+		"image": "succefull-0.png",
+		"image2x": "succefull-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 728,
+		"width": 1024,
+		"height": 40,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "succefull-1.png",
+		"image2x": "succefull-1@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 18,
+			"y": 86,
+			"width": 52,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"action": "back",
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 362,
+			"y": 589,
+			"width": 300,
+			"height": 52
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 10,
+"image": "typo.png",
+"image2x": "typo@2x.png",
+"width": 448,
+"height": 171,
+"title": "Typo",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+   ]
+,"resolutions": [2],
+"zoomEnabled": true,
+"title": "ui_IFE",
+"totalImages": 26,
+"highlightLinks": false
+}
